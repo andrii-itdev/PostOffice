a2f9f9b8a2be6419fd8a3863339a9d3a206e136b
andrii-itdev <andrii.itdev@gmail.com>
2021-03-16 22:56:18 +0200

    Add project files.

---
+++ PostOffice/EmailServiceModel.cs
@@ -0,0 +1,64 @@
+﻿using FluentEmail.Core;
+using FluentEmail.Smtp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace PostOffice
+{
+    public class EmailServiceModel
+    {
+        public MailAddress From { get; protected set; }
+        public string Password { get; protected set; }
+        public MailAddress To { get; protected set; }
+        public string Title { get; protected set; }
+        public string Content { get; protected set; }
+
+        public EmailServiceModel(string fromEmail, string password, string to, string title, string content)
+        {
+            this.From = new MailAddress(fromEmail);
+            this.Password = password;
+            this.To = new MailAddress(to);
+            this.Title = title;
+            this.Content = content;
+        }
+
+        public void TrySendEmail()
+        {
+            try
+            {
+                SendEmail();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show($"{ex.ToString()}", "Error sending email");
+            }
+        }
+
+        public void SendEmail()
+        {
+            var sender = new SmtpSender(() => new System.Net.Mail.SmtpClient("smtp." + From.Host)
+            {
+                EnableSsl = true,
+                DeliveryMethod = System.Net.Mail.SmtpDeliveryMethod.Network,
+                Credentials = new NetworkCredential(this.From.Address, this.Password),
+                Port = 587,
+            }
+            );
+
+            Email.DefaultSender = sender;
+
+            var email = Email
+                .From(this.From.Address)
+                .To(this.To.Address)
+                .Subject(this.Title)
+                .Body(this.Content)
+                .Send();
+        }
+    }
+}
---
+++ PostOffice/EmailSeviceViewModel.cs
@@ -0,0 +1,33 @@
+﻿using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace PostOffice
+{
+    public class EmailSeviceViewModel
+    {
+        public string From { get; set; }
+        public string Password { get; set; }
+        public string To { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        public ICommand SendCommand { get; set; }
+
+        public EmailSeviceViewModel()
+        {
+            SendCommand = new RelayCommand<object>(OnSendExecute);
+        }
+
+        private void OnSendExecute(object sender)
+        {
+            EmailServiceModel serviceModel = new EmailServiceModel( 
+                this.From, this.Password, this.To, this.Title, this.Content );
+            Task.Run(serviceModel.TrySendEmail);
+        }
+    }
+}
